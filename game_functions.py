f04078843375361795213d0f405849d5c96dd00e
froontown <froontown@froontowns-MacBook-Pro.local>
2017-04-13 12:35:56 -0400

    adding everything

---
+++ game_functions.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+
+def check_events(hero):
+    """Checks to see if a key is pressed."""
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            sys.exit()
+
+        # Let's make the hero run to the right and left!!!!
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                hero.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                hero.moving_left = True
+            elif event.key == pygame.K_UP:
+                hero.moving_up = True
+            elif event.key == pygame.K_DOWN:
+                hero.moving_down = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                hero.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                hero.moving_left = False
+            elif event.key == pygame.K_UP:
+                herp.moving_up = False
+            elif event.key == pygame.K_DOWN:
+                hero.moving_down = False
+
+
+def update_screen(ai_settings, screen, hero):
+    """Update the screen."""
+    # Redraw the screen during each pass through the loop
+    # screen.fill(ai_settings.bg_color) (not using a color)
+    hero.blitme()
+
+    # Display the most recently rendered screen
+    pygame.display.flip()
---
+++ hero.py
@@ -0,0 +1,47 @@
+import pygame
+
+class Hero():
+    """A class for the main character."""
+
+    def __init__(self, ai_settings, screen):
+        """Initialize the Hero!"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('assets/images/knight.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+        # For the decimals in hero's speed:
+        self.rect.centerx = float(self.rect.centerx)
+        self.rect.centery = float(self.rect.centery)
+
+    def update(self):
+        """Going to check the movement of the hero."""
+        if self.moving_right:
+            self.rect.centerx += self.ai_settings.hero_speed_factor
+
+        if self.moving_left:
+            self.rect.centerx -= self.ai_settings.hero_speed_factor
+
+        if self.moving_up:
+            self.rect.centery += self.ai_settings.hero_speed_factor
+
+        if self.moving_down:
+            self.rect.centery -= self.ai_settings.hero_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """Draws the hero!"""
+        self.screen.blit(self.image, self.rect)
